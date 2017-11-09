5a0fcd5f53b93d2dfadbe328cca619d8af452389
yangzai <1322133125@qq.com>
2017-11-09 12:47:43 +0800

    coder

---
+++ main.c
@@ -0,0 +1,21 @@
+/*************************************************************************
+    > File Name: main.c
+    > Author: YeKai
+    > Company: www.itcast.cn 
+    > Created Time: 2017年11月09日 星期四 12时43分51秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+int main(int argc,char *argv[])
+{
+    
+    return 0;
+
+}
